c2cb5f8e4f9285606a1288c69084d7c5dba52291
phillip-paine <pmxpp88@gmail.com>
2022-07-16 23:51:07 -0700

    Start of season history file started

---
+++ Code_Local/Code/start_of_season_strength.R
@@ -0,0 +1,4 @@
+## This script will calculate the team coefficents for the season ahead, including for newly promoted teams. 
+# Likely use some reversion to the mean for the start of the season, i.e. previous season estimates and then brought closer to the avg.
+# Promoted teams likely using estimate of promoted teams in the past. One thought is to use goals/concede from championship as a feature
+# to estimate start of season values.
