6af71a8d734a8c1436b20aebda981f991b626957
phillip-paine <54201799+phillip-paine@users.noreply.github.com>
2022-08-07 23:34:45 -0700

    Data to read 22-23 season (#4)
    * Update season 22/23
    
    Replace files calling 21-22 with 22-23 for updated data
    
    * Small tidying
    
    Tidy small commments

--- Code_Local/Code/make_datasets.R
+++ Code_Local/Code/make_datasets.R
@@ -1,5 +1,4 @@
 ## Creating datasets:
-# 08/11/2021
 # Phillip Paine
 
 # working code - change later
@@ -10,20 +9,19 @@ source('required_packages.R')
 ## READING DATA:
 
 # Pass url to read.csv function:
-RawPlayers <- read.csv("https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2021-22/players_raw.csv", header = TRUE)
+RawPlayers <- read.csv("https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2022-23/players_raw.csv", header = TRUE)
 # Create full name column as a unique identifier for players - need to adjust for duplicate names:
 RawPlayers <- RawPlayers %>% filter(id == 248) # Ben Davies but on loan at Sheffield Utd so can fudge this for now
 RawPlayers <- RawPlayers %>% mutate(full_name = paste(first_name, second_name, sep = "_"))
-# write.csv(RawPlayers, "D:/Phillip/GitHub/FantasyFootball2122/FF2021_Analysis/Data/RawPlayersDataset_endofseason.csv")
 
-CleanedPlayers <- read.csv("https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2021-22/cleaned_players.csv", header = TRUE)
+CleanedPlayers <- read.csv("https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2022-23/cleaned_players.csv", header = TRUE)
 CleanedPlayers <- CleanedPlayers %>% mutate(full_name = paste(first_name, second_name, sep = "_"))
 CleanedPlayers <- CleanedPlayers %>% filter(!(full_name == "Ben_Davies" & minutes == 0))
 
-IdPlayers <- read.csv("https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2021-22/player_idlist.csv", header = TRUE)
+IdPlayers <- read.csv("https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2022-23/player_idlist.csv", header = TRUE)
 CleanedPlayers <- merge(CleanedPlayers, IdPlayers, by = c("first_name", "second_name"), all.x = TRUE)
 
-TeamCode <- read.csv("https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2021-22/teams.csv", header = TRUE)
+TeamCode <- read.csv("https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2022-23/teams.csv", header = TRUE)
 
 # append to filenames to get team data:
 teams <- c("Arsenal", "Aston_Villa", "Brentford", "Brighton", "Burnley", "Chelsea", "Crystal_Palace", "Everton", "Leeds",  "Leicester", "Liverpool", 
@@ -33,13 +31,13 @@ teams <- c("Arsenal", "Aston_Villa", "Brentford", "Brighton", "Burnley", "Chelse
 for(t in teams){
   # replace space with underscore:
   t = sub(" ", "_", t)
-  assign(paste("stats", t, sep=""),read.csv(paste("https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2021-22/understat/understat_", t, ".csv", sep = ""),
+  assign(paste("stats", t, sep=""),read.csv(paste("https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2022-23/understat/understat_", t, ".csv", sep = ""),
                                             header = TRUE)) 
 }
 
-PlayersMatchData <- read.csv("https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2021-22/gws/merged_gw.csv", header = TRUE)
+PlayersMatchData <- read.csv("https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2022-23/gws/merged_gw.csv", header = TRUE)
 
-Fixtures <- read.csv("https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2021-22/fixtures.csv", header = TRUE)
+Fixtures <- read.csv("https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2022-23/fixtures.csv", header = TRUE)
 
 
 
